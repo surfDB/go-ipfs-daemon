41b73625e6268971bb3fc99dbfa7313881039075
Aditya Veer <nakulparmar15@gmail.com>
2022-09-21 15:57:06 +0530

    add ipfs s3 regision endpoint

--- config_scripts/Datastore.Spec.mounts.s3.sh
+++ config_scripts/Datastore.Spec.mounts.s3.sh
@@ -7,6 +7,7 @@ echo "[
             \"region\": \"$IPFS_S3_REGION\",
             \"bucket\": \"$IPFS_S3_BUCKET_NAME\",
             \"rootDirectory\": \"$IPFS_S3_ROOT_DIRECTORY\",
+            \"regionEndpoint\": \"$IPFS_S3_REGION_ENDPOINT\",
             \"accessKey\": \"$IPFS_S3_ACCESS_KEY_ID\",
             \"secretKey\": \"$IPFS_S3_SECRET_ACCESS_KEY\",
             \"keyTransform\": \"$IPFS_S3_KEY_TRANSFORM\"
